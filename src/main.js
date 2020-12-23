4891a3c809ea03f50c542c3347bf6e7d08302e23
Rhys Stever <rhys@stever.com>
2020-12-23 14:53:00 -0800

    Switched to using grids instead of flexboxes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+// *** This version is for a xmas gift for a friend of mine *** 
+// It is intentionally made to look like the game: 4 Pics 1 Word
+
 const answer = "BASS";
 const answerLength = answer.length;
-const letters = ['F', 'S', 'R', 'G', 'C', 'B', 
+let letters = ['F', 'S', 'R', 'G', 'C', 'B', 
 				'S', 'K', 'E', 'A', 'L', 'U'];
 const fps = 20;
 let isPlaying;
@@ -32,23 +35,24 @@ function setupButtons(){
 	let answerDiv = document.querySelector("#answer");
 	for(let l = 0; l < answerLength; l++){
 		let letter = document.createElement("button");
+		let text = document.createElement("p");
+		text.className = "bold";
 		letter.innerHTML = "";
-		letter.className = "letter btn btn-secondary";
+		letter.className = "letter btn btn-light";
 		letter.onclick = answerBoxClicked;
+		letter.appendChild(text);
 		answerDiv.appendChild(letter);
 	}
 	
 	// Clickable letters setup
 	let lettersDiv = document.querySelector("#letters");
-	for(let l = 0; l < lettersDiv.children.length; l++){
-		lettersDiv.children[l].innerHTML = letters[l];
+	for(let l = 0; l < letters.length; l++){
+		let text = lettersDiv.children[l].firstChild;
+		text.innerHTML = letters[l];
+		text.classList.add('bold');
 		lettersDiv.children[l].id = "letter" + l;
 		lettersDiv.children[l].onclick = letterClicked;
-	}
-
-	let resetButton = document.querySelector("#resetButton");
-	resetButton.onclick = (e) => {
-		location.reload();
+		lettersDiv.children[l].appendChild(text);
 	}
 }
 
@@ -64,43 +68,55 @@ function pictureClicked(e){
 function answerBoxClicked(e){
 	if(!isPlaying) return;
 
+	// Finds the text object of the button
+	let text = e.target;
+	if(text.classList.contains("letter")){
+		text = e.target.firstChild;
+	}
+
 	// Removes the text from the answer box and
 	// finds the letter button and makes it visible once more
-	e.target.innerHTML = "";
+	text.innerHTML = "";
 	let lettersDiv = document.querySelector("#letters");
 	for(let l = 0; l < lettersDiv.children.length; l++){
-		if(lettersDiv.children[l].id == e.target.id)
+		if(lettersDiv.children[l].id == text.id){
 			lettersDiv.children[l].style.visibility = "visible";
+		}
 	}
-	e.target.removeAttribute('id');
+	text.removeAttribute('id');
 }
 
 function letterClicked(e){
 	if(!isPlaying) return;
+
+	let button  = e.target;
+	if(button.classList.contains("bold")){
+		button = e.target.parentElement;
+	}
 
 	// Finds the leftmost open answer box and 
 	// fills it in with the clicked letter and
 	// sets its id to the same id as the clicked button
 	let answerDiv = document.querySelector("#answer");
 	for(let c = 0; c < answerDiv.children.length; c++){
-		if(answerDiv.children[c].innerHTML == ""){
-			answerDiv.children[c].innerHTML = e.target.innerHTML;
-			answerDiv.children[c].id = e.target.id;
+		if(answerDiv.children[c].firstChild.innerHTML == ""){
+			answerDiv.children[c].firstChild.innerHTML = button.firstChild.innerHTML;
+			answerDiv.children[c].firstChild.id = button.id;
 			break;
 		}
 	}
-
+	
 	// Makes the clicked letter vanish
-	e.target.style.visibility = "hidden";
+	button.style.visibility = "hidden";
 }
 
 function checkAnswer(){
 	// Returns FALSE if there are blanks in any of the answer boxes
 	let answerDiv = document.querySelector("#answer");
 	for(let c = 0; c < answerDiv.children.length; c++){
-		if (answerDiv.children[c].innerHTML == ""){
+		if (answerDiv.children[c].children[0].innerHTML == ""){
 			for(let c = 0; c < answerDiv.children.length; c++)
-				answerDiv.children[c].className = "letter btn btn-secondary";
+				answerDiv.children[c].className = "letter btn btn-light";
 			return false;
 		}
 	}
@@ -108,7 +124,7 @@ function checkAnswer(){
 	// "Contructs" the guess based on the letters in each answer box
 	let guess = "";
 	for(let c = 0; c < answerDiv.children.length; c++)
-		guess += answerDiv.children[c].innerHTML;
+		guess += answerDiv.children[c].children[0].innerHTML;
 	
 	// Returns whether the guess is the same as the answer
 	// and changes the color of the text
